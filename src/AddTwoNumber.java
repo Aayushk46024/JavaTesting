8c5e9ebcca058684a1a1f6be5e3e56da4d46de60
Aayushk46024 <aayush.khandelwal@infostride.com>
2022-01-04 09:10:26 +0530

    Initial commit

--- src/AddTwoNumber.java
+++ src/AddTwoNumber.java
@@ -1,7 +1,7 @@
 public class AddTwoNumber {
     public static void main(String []args)
     {
-        int a = 10, b = 20, sum;
+        int a = 10, b = 50, sum;
         sum = a + b;
         System.out.println("Sum is: "+ sum);
     }
