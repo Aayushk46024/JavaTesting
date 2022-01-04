839bc673315e08bf0c91f33f618c3d0a4c0e7a97
Aayushk46024 <aayush.khandelwal@infostride.com>
2022-01-04 09:08:41 +0530

    Initial commit

---
+++ src/AddTwoNumber.java
@@ -0,0 +1,8 @@
+public class AddTwoNumber {
+    public static void main(String []args)
+    {
+        int a = 10, b = 20, sum;
+        sum = a + b;
+        System.out.println("Sum is: "+ sum);
+    }
+}
